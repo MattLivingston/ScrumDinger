eb69f9d906ab7a02136b94081cce6d163d067672
Matt Livingston <mlivingston2066@gmail.com>
2023-02-17 19:41:40 -0600

    Chapter 2, Section 3

---
+++ ScrumDinger/ScrumDinger/CardView.swift
@@ -0,0 +1,8 @@
+//
+//  CardView.swift
+//  ScrumDinger
+//
+//  Created by Matt Livingston on 2/17/23.
+//
+
+import Foundation
--- ScrumDinger/ScrumDinger/MeetingView.swift
+++ ScrumDinger/ScrumDinger/MeetingView.swift
@@ -25,6 +25,10 @@ struct MeetingView: View {
                     Label("600", systemImage: "hourglass.tophalf.fill")
                 }
             }
+            .accessibilityElement(children: .ignore)
+            .accessibilityLabel("Time remaining")
+            .accessibilityValue("10 minutes")
+            .accessibilityValue("10 minutes")
             Circle()
                 .strokeBorder(lineWidth: 24)
             HStack{
@@ -33,6 +37,7 @@ struct MeetingView: View {
                 Button(action: {}) {
                     Image(systemName: "forward.fill")
                 }
+                .accessibilityLabel("Next speaker")
             }
         }
         .padding()
